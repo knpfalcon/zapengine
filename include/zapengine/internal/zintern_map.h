0471d6006b96bca43a707638a316e633d9b16731
knpfalcon <knpfalcon@gmail.com>
2023-05-28 17:54:10 -0400

    Started on map struct. This might be a play-by-ear thing.

--- include/zapengine/internal/zintern_map.h
+++ include/zapengine/internal/zintern_map.h
@@ -1,6 +1,36 @@
 #ifndef ZINTERN_MAP_H
 #define ZINTERN_MAP_H
 
+#include <stdbool.h>
 #include "map.h"
+#include "zapengine/internal/zintern_actor.h"
+
+struct ZAP_MAP_TILE
+{
+    int type;
+};
+
+struct ZAP_MAP_ACTOR
+{
+    int type;
+};
+
+struct ZAP_MAP
+{
+    char name[32];
+    int bgm;
+
+    int w;
+    int h;
+
+    //Layers
+    ZAP_MAP_TILE layer_bg[MAX_MAP_SIZE];
+    ZAP_MAP_TILE layer_fg[MAX_MAP_SIZE];
+    ZAP_MAP_TILE layer_clsn[MAX_MAP_SIZE];
+
+    //Actors
+    int actor_count;
+    ZAP_MAP_ACTOR actors[MAX_ACTORS];
+};
 
 #endif
--- include/zapengine/map.h
+++ include/zapengine/map.h
@@ -1,4 +1,11 @@
 #ifndef MAP_H
 #define MAP_H
 
+#define MAX_MAP_SIZE 11520
+#define MAX_TILES 512
+
+typedef struct ZAP_MAP_TILE ZAP_MAP_TILE;
+typedef struct ZAP_MAP_ACTOR ZAP_MAP_ACTOR;
+typedef struct ZAP_MAP ZAP_MAP;
+
 #endif
